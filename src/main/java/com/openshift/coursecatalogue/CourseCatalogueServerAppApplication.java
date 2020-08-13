2fe23ab35949e306c624d7be5bde32aaba9f8aee
Manohari21 <52821947+Manohari21@users.noreply.github.com>
2020-08-13 16:13:42 +0530

    commit

--- src/main/java/com/openshift/coursecatalogue/CourseCatalogueServerAppApplication.java
+++ src/main/java/com/openshift/coursecatalogue/CourseCatalogueServerAppApplication.java
@@ -2,13 +2,21 @@ package com.openshift.coursecatalogue;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CourseCatalogueServerAppApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/");
 		SpringApplication.run(CourseCatalogueServerAppApplication.class, args);
 	}
-
+	/*
+	 * @Bean public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory>
+	 * webServerFactoryCustomizer() { return factory -> factory.setContextPath("/");
+	 * }
+	 */
 }
